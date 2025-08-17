43dc1d20abd01764ba36b8441cf413d118fb3b4e
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2025-08-17 20:29:50 -0300

    feat: init resp func readLine

---
+++ resp.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"io"
+)
+
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+
+	for {
+
+		// b, err := r.reader.
+	}
+}
